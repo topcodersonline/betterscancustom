e1630a819143ad4a051921167084a636cd825852
Your Name <you@example.com>
2022-07-28 21:28:05 +0200

    Init

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+export CODE_DIR=${PWD}
+cd $CODE_DIR
+cd $CODE_DIR && checkmate init
+cd $CODE_DIR && checkmate git init
+cd $CODE_DIR && checkmate git analyze --branch $1
+cd $CODE_DIR && checkmate issues html
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+git init && git add . && git commit -m "state"
+git branch $1
+git push origin $1
