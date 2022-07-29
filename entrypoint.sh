46bc2951781bfb1215f5684ecfde18842e942725
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-29 12:45:14 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ cd $CODE_DIR
 cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze --branch $1
+ls -la $CODE_DIR
 cd $CODE_DIR && checkmate issues html
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
