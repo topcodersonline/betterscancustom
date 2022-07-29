bf3728e76a443cf377b584a5f8e5b9f90b785ce0
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-29 12:59:41 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,13 @@
 #!/bin/sh -l
 
 export CODE_DIR="/github/workspace"
-echo $CODE_DIR
-ls -la $CODE_DIR
 git checkout $1
 cd $CODE_DIR
 cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze --branch $1
-ls -la $CODE_DIR
 cd $CODE_DIR && checkmate issues html
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 git init && git add . && git commit -m "state"
-git branch $1
 git push origin $1
