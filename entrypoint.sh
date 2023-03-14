ea335860221050d935a7c57a2a4ddb151a491e4e
Marcin Kozlowski <marcinguy@gmail.com>
2023-03-14 09:44:09 +0100

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze --branch $1
 cd $CODE_DIR && checkmate issues html
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+git config --global user.email "bot@betterscan.io"
+git config --global user.name "Betterscan.io Bot"
 git init && git add . && git commit -m "state"
 git push origin $1
